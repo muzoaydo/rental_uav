ad728f2af9d040f934fa11a833a0141947d57001
muzoaydo <muzo1998@hotmail.com>
2023-03-07 02:54:15 +0300

    Created UAV class in 'models.py'

--- uav/models.py
+++ uav/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+# UAV class with necessary fields.
+class UAV(models.Model):
+    brand = models.CharField(max_length=50)
+    model = models.CharField(max_length=50)
+    weight = models.FloatField(max_length=50)
+    category = models.CharField(max_length=50)
+    rented = models.BooleanField()
+
+    def __str__(self):
+        return self.brand + ' ' + self.model
