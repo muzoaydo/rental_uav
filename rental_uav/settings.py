85da2157a566179c90a7875dc83037afefb3664f
muzoaydo <muzo1998@hotmail.com>
2023-03-07 03:02:12 +0300

    fix on app name on settings.py.

--- rental_uav/settings.py
+++ rental_uav/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'corsheaders',
-    'uav.apps.uavConfig'
+    'uav.apps.UavConfig'
 ]
 
 # CORS_ORIGIN_ALLOW_ALL = True
