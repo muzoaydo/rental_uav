ad80618e5575a5e2e569737e5d29172d60dd41a2
muzoaydo <muzo1998@hotmail.com>
2023-03-07 04:52:19 +0300

    First viable product for the APIs.

--- uav/urls.py
+++ uav/urls.py
@@ -3,6 +3,6 @@ from uav import views
 
 # URL paths for UAV pages
 urlpatterns=[
-    path('uav/', views.UAVList.as_view(), name='uav-list'),
+    path('uavApi/', views.UAVList.as_view(), name='uav-list'),
     path('uav/<int:pk>/', views.UAVDetail.as_view(), name='uav-detail'),
 ]
