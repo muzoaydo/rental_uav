66b3ed7702bdc2ecb818e9e85a55f40936d733b9
muzoaydo <muzo1998@hotmail.com>
2023-03-07 03:00:49 +0300

    Made DB configurations on 'settings.py'.

--- rental_uav/settings.py
+++ rental_uav/settings.py
@@ -81,8 +81,10 @@ WSGI_APPLICATION = 'rental_uav.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'rental_uav',
+        'USER': 'postgres',
+        'PASSWORD': '1234'
     }
 }
 
