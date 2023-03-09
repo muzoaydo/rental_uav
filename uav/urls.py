15006452a7b0cdb06a48fabb64d59640cde83926
muzoaydo <muzo1998@hotmail.com>
2023-03-09 03:50:13 +0300

    Added crud urls to 'uav/urls.py'

--- uav/urls.py
+++ uav/urls.py
@@ -4,9 +4,10 @@ from django.views.generic.base import TemplateView
 
 # URL paths for UAV pages
 urlpatterns=[
-    path('uavApi/', views.UAVList.as_view(), name='uav_list'),
-    path('uavApi/<int:id>/', views.UAVDetail.as_view(), name='uav_detail'),
-    path('uav/', TemplateView.as_view(template_name='uav_list.html'), name='uav_list'),
-    path('uav_create/', TemplateView.as_view(template_name='uav_create.html'), name='uav_create'),
-    path('uav_update/', TemplateView.as_view(template_name='uav_update.html'), name='uav_update'),
+    path('uavApi/', views.UAVAPIList.as_view(), name='uav_api'),
+    path('uavApi/<int:pk>/', views.UAVAPIDetail.as_view(), name='uav_detail'),
+    path('hireUAV/', views.uavList, name='uav_list'),
+    path('uav_create/', views.uavCreate, name='uav_create'),
+    path('uav_update/<int:id>/', views.uavUpdate, name='uav_update'),
+    path('uav_delete/<int:id>/', views.uavDelete, name='uav_delete'),
 ]
