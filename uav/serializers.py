9e028d83f54874a26eef8fcbb89f979d022656ce
muzoaydo <muzo1998@hotmail.com>
2023-03-09 03:49:45 +0300

    Added UAVForm to 'serializers.py'

--- uav/serializers.py
+++ uav/serializers.py
@@ -1,8 +1,14 @@
 from rest_framework import serializers
+from django.forms import ModelForm
 from uav.models import UAV
 
 # Serializer for UAV
 class UAVSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = UAV
+        fields = '__all__'
+
+class UAVForm(ModelForm):
     class Meta:
         model = UAV
         fields = '__all__'
